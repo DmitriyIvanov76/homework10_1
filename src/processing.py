c0e51b11a1bd0067143eb7e4e53b52b3467645ca
Dmitriy Ivanov <dmtr_ivanov@outlook.com>
2025-02-09 23:02:40 +0300

    fix readme.md

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,12 @@
-def filter_by_state(incoming_data: list[dict], state='EXECUTED') -> list[dict]:
+def filter_by_state(incoming_data: list[dict], state="EXECUTED") -> list[dict]:
     """функция возвращает список в зависимости от состояния"""
     result = list(filter(lambda x: x["state"] in state, incoming_data))
     return result
 
 
-def sort_by_date (incoming_data: list[dict], sorted_parameter=True) -> list[dict]:
+def sort_by_date(incoming_data: list[dict], sorted_parameter=True) -> list[dict]:
     """функция сортировки данных по дате"""
-    result = list(sorted(incoming_data, key=lambda x: x['date'], reverse=sorted_parameter))
+    result = list(
+        sorted(incoming_data, key=lambda x: x["date"], reverse=sorted_parameter)
+    )
     return result
-
