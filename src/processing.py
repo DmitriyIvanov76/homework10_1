5c5d38d74e0edc0aa6e3efc13be3893f5a96eabc
Dmitriy Ivanov <dmtr_ivanov@outlook.com>
2025-02-10 01:11:39 +0300

    homework 10.1 fix

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,14 @@
-def filter_by_state(incoming_data: list[dict], state="EXECUTED") -> list[dict]:
-    """функция возвращает список в зависимости от состояния"""
+def filter_by_state(incoming_data: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """the function returns a list depending on the state"""
     result = list(filter(lambda x: x["state"] in state, incoming_data))
     return result
 
 
-def sort_by_date(incoming_data: list[dict], sorted_parameter=True) -> list[dict]:
-    """функция сортировки данных по дате"""
+def sort_by_date(
+    incoming_data: list[dict], sorted_parameter: bool = True
+) -> list[dict]:
+    """function to sort data by date"""
     result = list(
         sorted(incoming_data, key=lambda x: x["date"], reverse=sorted_parameter)
     )
     return result
-
